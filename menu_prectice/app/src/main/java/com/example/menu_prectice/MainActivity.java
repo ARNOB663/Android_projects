acbc90bdef09c2264bcf42588e7dee639fb9eb91
Aniruddha Dev Nath <223002912@eastdelta.edu.bd>
2023-11-18 10:32:58 +0600

    a

--- menu_prectice/app/src/main/java/com/example/menu_prectice/MainActivity.java
+++ menu_prectice/app/src/main/java/com/example/menu_prectice/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.menu_prectice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
-}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+/*        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);*/
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+
+    }
+
+
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+
+        //noinspection SimplifiableIfStatement
+
+
+
+        //---------------------------
+
+
+    }
+
+    //----------onclick
+
+
