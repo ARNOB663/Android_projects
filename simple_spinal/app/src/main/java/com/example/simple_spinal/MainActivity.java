ddf074f17ed60d6b85278dc477c3397a40ab046c
Aniruddha Dev Nath <223002912@eastdelta.edu.bd>
2023-11-24 12:33:21 +0600

    a

--- simple_spinal/app/src/main/java/com/example/simple_spinal/MainActivity.java
+++ simple_spinal/app/src/main/java/com/example/simple_spinal/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.simple_spinal;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Spinner lv1;
@@ -15,12 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lv1=findViewById(R.id.spinner1);
-        Resources res =getResources();
-        CTGTHANA = res.getStringArray(R.array.CTGTHANA);
-
-        //String[] name = {"Ford","Mercedes-Benz","General Motors","BMW","Audi","Honda","Chevrolet","Hyundai","TOYOTA","Kia","Cadillac","Tesla","Volkswagen","Lexus","Jeep","Nissan"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.custom_diesne,R.id.textView,CTGTHANA);
+        lv1 = findViewById(R.id.spinner1);
+        Resources res = getResources();
+        String[] name = {"Ford","Mercedes-Benz","General Motors","BMW","Audi","Honda","Chevrolet","Hyundai","TOYOTA","Kia","Cadillac","Tesla","Volkswagen","Lexus","Jeep","Nissan"};
+        //CTGTHANA = res.getStringArray(R.array.CTGTHANA);
+        List<String> listca;
+        listca = new ArrayList<>();
+        for (String i: name)
+        {
+            listca.add(i);
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.custom_diesne,R.id.textView,listca);
         lv1.setAdapter(adapter);
         //lv2=findViewById(R.id.spinner);
     }
