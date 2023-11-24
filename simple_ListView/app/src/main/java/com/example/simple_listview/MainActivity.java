eaf273768e036176090ca9519bc1ae179a89af62
Aniruddha Dev Nath <223002912@eastdelta.edu.bd>
2023-11-24 10:59:59 +0600

    a

--- simple_ListView/app/src/main/java/com/example/simple_listview/MainActivity.java
+++ simple_ListView/app/src/main/java/com/example/simple_listview/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 ListView lv1;
+ListView lv2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +17,10 @@ ListView lv1;
         String[] name = {"Ford","Mercedes-Benz","General Motors","BMW","Audi","Honda","Chevrolet","Hyundai","TOYOTA","Kia","Cadillac","Tesla","Volkswagen","Lexus","Jeep","Nissan"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1,name);
         lv1.setAdapter(adapter);
+        lv2=findViewById(R.id.list_view2);
+        Integer[] name2={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,20};
+        ArrayAdapter<Integer> adapter1 = new ArrayAdapter<Integer>(getApplicationContext(),android.R.layout.simple_list_item_1,name2);
+        lv2.setAdapter(adapter1);
+
     }
 }
