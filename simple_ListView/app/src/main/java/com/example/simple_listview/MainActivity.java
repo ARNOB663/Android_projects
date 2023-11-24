ce4bb7c93d2443e5542b0efe12b3431a18168208
Aniruddha Dev Nath <223002912@eastdelta.edu.bd>
2023-11-24 11:30:04 +0600

    Update MainActivity.java

--- simple_ListView/app/src/main/java/com/example/simple_listview/MainActivity.java
+++ simple_ListView/app/src/main/java/com/example/simple_listview/MainActivity.java
@@ -21,7 +21,7 @@ ListView lv2;
         CTGTHANA = res.getStringArray(R.array.CTGTHANA);
 
         //String[] name = {"Ford","Mercedes-Benz","General Motors","BMW","Audi","Honda","Chevrolet","Hyundai","TOYOTA","Kia","Cadillac","Tesla","Volkswagen","Lexus","Jeep","Nissan"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, CTGTHANA);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1,CTGTHANA);
         lv1.setAdapter(adapter);
         lv2=findViewById(R.id.list_view2);
         Integer[] name2={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,20};
