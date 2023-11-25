a53ec0115bf63b60ec54230f7ff81016f91a3657
Aniruddha Dev Nath <223002912@eastdelta.edu.bd>
2023-11-25 10:44:46 +0600

    Update MainActivity.java

--- creating_list_viewUsingListActivity/app/src/main/java/com/example/creating_list_viewusinglistactivity/MainActivity.java
+++ creating_list_viewUsingListActivity/app/src/main/java/com/example/creating_list_viewusinglistactivity/MainActivity.java
@@ -8,11 +8,13 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends ListActivity {
     String[] marray= {"abrar","jawad","asif","anika","muskhy","bijoy","aqila","faqlia","saqila","maqila"};
     ListView lv;
+    TextView text1;
     ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +30,8 @@ public class MainActivity extends ListActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String post= marray[position];
                 Toast.makeText(MainActivity.this,post+"position is"+position,Toast.LENGTH_SHORT).show();
+                TextView text1 = findViewById(R.id.textView);
+                text1.setText(marray[position]);
             }
         });
     }
