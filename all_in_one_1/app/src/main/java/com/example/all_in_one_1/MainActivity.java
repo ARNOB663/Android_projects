652ef4a2d1332a6d592f9cdc91b5c1217101d949
Aniruddha Dev Nath <223002912@eastdelta.edu.bd>
2023-11-28 11:12:51 +0600

    a

--- all_in_one_1/app/src/main/java/com/example/all_in_one_1/MainActivity.java
+++ all_in_one_1/app/src/main/java/com/example/all_in_one_1/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 ListView lv1;
@@ -24,6 +25,7 @@ String[] item;
         lv1 = findViewById(R.id.list_view);
         edit_text = findViewById(R.id.entrydata);
 
+
         Resources res =getResources();
         item = res.getStringArray(R.array.item);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1,item);
@@ -32,12 +34,13 @@ String[] item;
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 if(position==0)
-                {
+                {    Toast.makeText(getApplicationContext(),"picture",Toast.LENGTH_SHORT).show();
                     Intent myIntent=new Intent(view.getContext(),farrai_488_short.class);
                     startActivity(myIntent);
+
                 }
                 if(position==2)
-                {
+                { Toast.makeText(getApplicationContext(),"camera",Toast.LENGTH_SHORT).show();
                     Intent i1= new Intent();
                     //i1.setAction(MediaStore.ACTION_VIDEO_CAPTURE);
                     i1.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -45,11 +48,12 @@ String[] item;
                 }
                 if(position==3)
                 {
-                    Intent i1 = new Intent();
-                    i1.setAction(Intent.ACTION_VIEW);
-                    i1.setData(Uri.parse("https:" + edit_text.getText()));
-                    //i1.setData(Uri.parse("tel:" + edit_text.getText()));
-                    startActivity(i1);
+                    Toast.makeText(getApplicationContext(),"Going to : "+edit_text.getText(),Toast.LENGTH_SHORT).show();
+                    Intent ii = new Intent();
+                    ii.setAction(Intent.ACTION_VIEW);
+//                ii.setData(Uri.parse("https://www.google.com/"));//For static
+                    ii.setData(Uri.parse("https:" + edit_text.getText()));//For dynamic
+                    startActivity(ii);
                 }
             }
         });
@@ -57,12 +61,12 @@ String[] item;
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 if(position==0)
-                {
+                {  Toast.makeText(getApplicationContext(),"information",Toast.LENGTH_SHORT).show();
                     Intent myIntent=new Intent(view.getContext(),farrai_488_long.class);
                     startActivity(myIntent);
                 }
                 if(position==2)
-                {
+                {  Toast.makeText(getApplicationContext(),"video",Toast.LENGTH_SHORT).show();
                     Intent i1= new Intent();
                     i1.setAction(MediaStore.ACTION_VIDEO_CAPTURE);
                     //i1.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
