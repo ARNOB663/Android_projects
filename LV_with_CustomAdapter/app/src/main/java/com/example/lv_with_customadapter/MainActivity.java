3a05db68b968be052577c04544a30f2df2e5ed69
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2023-12-12 14:21:06 +0600

    a

--- LV_with_CustomAdapter/app/src/main/java/com/example/lv_with_customadapter/MainActivity.java
+++ LV_with_CustomAdapter/app/src/main/java/com/example/lv_with_customadapter/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.lv_with_customadapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Spinner;
 
@@ -12,9 +15,9 @@ public class MainActivity extends AppCompatActivity {
 
     Spinner sp;
 
-    String[] district = {"GTA 6 release date year confirmed by first trailer","Chittagong","Rangpur","Sylhet","Mymensingh"};
+    String[] district = {"GTA 6 release date year confirmed by first trailer","Rumor: Possible PS5 Pro Specs Leak Online","RED DEAD REDEMPTION 3 ‘OFFICIALLY IN THE WORKS’, IT’S CLAIMED","There Are Some Amazing PC Game Deals In Fanatical's Winter Sale"};
 
-    Integer[] ImageId ={R.drawable.gta6,R.drawable.chittagong,R.drawable.rangpur,R.drawable.sylhet,R.drawable.mymensingh};
+    Integer[] ImageId ={R.drawable.gta6,R.drawable.ps5_pro,R.drawable.rdr3,R.drawable.g1};
 
     //String[] area = {"306.4 km²","168.1 km²","2,401 km²","26.5 km²","91.32 km²"};
 
@@ -33,5 +36,30 @@ public class MainActivity extends AppCompatActivity {
         sp = (Spinner) findViewById(R.id.sp1);
         CustomAdapter listAdapter1 = new CustomAdapter(MainActivity.this, district,ImageId);
         sp.setAdapter(listAdapter1);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if( position == 0)
+                {
+                    Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                    startActivity(intent);
+                }
+                if (position == 1)
+                {
+                    Intent intent = new Intent(MainActivity.this, MainActivity3.class);
+                    startActivity(intent);
+                }
+                if (position == 2)
+                {
+                    Intent intent = new Intent(MainActivity.this, MainActivity4.class);
+                    startActivity(intent);
+                }
+                if (position == 3)
+                {
+                    Intent intent = new Intent(MainActivity.this, MainActivity5.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
