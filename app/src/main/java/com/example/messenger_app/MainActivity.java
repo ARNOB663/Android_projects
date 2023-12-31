c32b2d2d1581ca27a7e0877d3d64bc0ee3a549be
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2023-12-31 23:38:08 +0600

    a

--- app/src/main/java/com/example/messenger_app/MainActivity.java
+++ app/src/main/java/com/example/messenger_app/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.messenger_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;// = FirebaseAuth.getInstance(); // get an instance of the firebase authentication service
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();// get an instance of the firebase authentication service
+        if(auth.getCurrentUser() == null) {
+            Intent intent = new Intent(MainActivity.this, login.class); // create an intent to start the login activity
+            startActivity(intent);
     }
 }
