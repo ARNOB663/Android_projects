6d23c72bbf06198addf3e91bb5f7b3a1533ceed6
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-01-03 13:15:52 +0600

    a

--- RecycelerView_pojo_sample/app/src/main/java/com/example/recycelerview_pojo_sample/MainActivity.java
+++ RecycelerView_pojo_sample/app/src/main/java/com/example/recycelerview_pojo_sample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.recycelerview_pojo_sample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -17,18 +18,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setAdapter(new CustomAdapter(this, list));
-
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         list = new ArrayList<>(); // create a list of Model class
+        list.add(new Model("1", "Koenigsegg Agera RS", "Koenigsegg Agera RS ($2,550,000)", R.drawable.koenigsegg, 4.3, 	2550000));
+       list.add(new Model("2", "Lamborghini Aventador", "Lamborghini Aventador ($393,695)", R.drawable.lamborghini, 4.5, 	393695));
+ list.add(new Model("3", "Bugatti Chiron", "Bugatti Chiron ($2,998,000)", R.drawable.bugatti_chiron, 4.7, 	2998000));
+ list.add(new Model("4", "aston martin", "aston martin ($1,000,000)", R.drawable.aston_martin_vulcan, 4.3, 	1000000));
+ list.add(new Model("5", "Ferrari LaFerrari", "Ferrari LaFerrari ($1,416,362)", R.drawable.ferrari_laferrari, 4.5, 	1416362));
+ list.add(new Model("6", "Pagani Huayra", "pagani ($1,400,000)", R.drawable.pagani_Huayra, 4.7, 	1400000));
+ list.add(new Model("7", "McLaren P1", "McLaren P1 ($1,350,000)", R.drawable.mclaren_p1, 4.3, 	1350000));
 
-        list.add(new Model("1", "Koenigsegg Agera RS", "This is product 1", R.drawable.koenigsegg, 4.3, 	2550000));
-
-
+ CustomAdapter adapter = new CustomAdapter(this, list);
+recyclerView.setAdapter(adapter);
     }
 }
